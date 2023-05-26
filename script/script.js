7f6290fa90963b06055d183dd013fa619efadefb
maxberglund2 <tf2bro999@gmail.com>
2023-05-26 20:18:00 +0200

    Fixed the calculator.

--- script/script.js
+++ script/script.js
@@ -5,9 +5,15 @@ function convert() {
     let power = (temp * 1.16194) / 1000;
     let x = hr * 3.760;
     let cost = power * x; 
-    document.getElementsByName('power')[0].value = power + " kWh";
-    document.getElementById('result').innerText = cost + " kr";
-    document.getElementById('hide1').style.display = "block";
-    document.getElementById('result').style.display = "block";
-    document.getElementById('margin').style.marginBottom = "20px";
+    if(temp > - 1 && hr > - 1 && time != "" && hr != "")
+    {
+        document.getElementsByName('power')[0].value = power + " kWh";
+        document.getElementById('result').innerText = cost + " kr";
+        document.getElementById('hide1').style.display = "block";
+        document.getElementById('result').style.display = "block";
+        document.getElementById('margin').style.marginBottom = "20px";
+    } else
+    {
+        alert("Invalid Input!");
+    }
 }
