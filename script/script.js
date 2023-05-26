6b729ae1b5bb10841ab85fd6cef4bfcd2e5e5eec
maxberglund2 <tf2bro999@gmail.com>
2023-05-26 19:23:34 +0200

    all

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function convert() {
+    event.preventDefault(); // Refresha inte sidan
+    let temp = document.getElementById('temp').value;
+    let hr = document.getElementById('time').value;
+    let power = (temp * 1.16194) / 1000;
+    let x = hr * 3.760;
+    let cost = power * x; 
+    document.getElementsByName('power')[0].value = power + " kWh";
+    document.getElementById('result').innerText = cost + " kr";
+    document.getElementById('hide1').style.display = "block";
+    document.getElementById('result').style.display = "block";
+    document.getElementById('margin').style.marginBottom = "20px";
+}
